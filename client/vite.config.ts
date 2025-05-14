ef4a82f704705d7bfc02140d6b4519ab372cbe80
Utkarsh Gupta <guptautkarsh2312@outlook.com>
2025-05-14 18:05:49 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:3001',
+          changeOrigin: true
+        }
       }
     }
   }
